6a16a4f8830fd00ea7017721272eb64cdb317672
Paul Goller <hmp@h4emp3.net>
2013-10-17 00:08:52 +0200

    add first version of styled forms

---
+++ page-contact.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying the contact page.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage EchosOratos
+ * @since EchosOratos 0.1
+ */
+
+get_header(); ?>
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<article id="page-contact post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <?php the_content(); ?>
+				</article>
+			<?php endwhile; ?>
+		</div>
+	</div>
+<?php get_footer(); ?>
