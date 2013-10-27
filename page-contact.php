d2bcf4b1bcf5f0cd8356f20327ecec002b509cff
Paul Goller <hmp@h4emp3.net>
2013-10-28 00:37:53 +0100

    add styling for form error handling

--- page-contact.php
+++ page-contact.php
@@ -15,7 +15,7 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
-				<article id="page-contact post-<?php the_ID(); ?>" <?php post_class('no-header'); ?>>
+				<article id="page-contact" <?php post_class('no-header'); ?>>
                     <?php the_content(); ?>
 					<footer class="entry-meta">
 						<?php edit_post_link( __( 'Edit', 'twentythirteen' ), '<span class="edit-link">', '</span>' ); ?>
